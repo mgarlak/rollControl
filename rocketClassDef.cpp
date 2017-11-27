34c0d31864e5b0ffc216fe1c2bb5bb0140890acc
lunar-mycroft <timothy.l.park+github@gmail.com>
2017-11-27 15:07:46 -0500

    Redefined roll
    Added functions to calculate roll based on new definition which should work well when the rocket is near vertical (which it should be for most of the flight).

--- rocketClassDef.cpp
+++ rocketClassDef.cpp
@@ -16,3 +16,20 @@ int rocket::updateSensorData(Adafruit_BMP280 bmp, Adafruit_BNO055 orient){
 
   z = bmp.readAltitude(1013.25 /*HARDCODED, WE'LL CHANGE LATER*/);
 }
+
+double rocket::getPitch(){
+    if (!pitchUp2Date){
+        pitch=/*(right angle) - arccos(The dot product of the rocket-frame z unit vector and the ground frame z unit vector)*/;
+    }
+    return pitch;
+}
+double rocket::getRoll(){
+    if(!rollUp2Date){
+        roll=/*angle between the projection of the x unit vector in the rocket frame on the ground frame xy plane and the ground frame x direction*/;
+    }
+    return roll;
+}
+
+double rocket::getRollRate(){
+    //Should be nearly identical to get roll, except using vQ instead of Q. 
+}
