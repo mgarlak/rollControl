8e0906e2b9d18cd08fc03a246864c9e16112637b
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2018-02-23 16:54:30 -0500

    Implimented proper roll calculations

--- control/rocketClassDef.cpp
+++ control/rocketClassDef.cpp
@@ -66,7 +66,7 @@ float rocket::getPitch(){
         rocketUp[3]={0};
         Matrix.Multiply((float *)R,(float *)up,3,3,1,(float*)rocketUp);
 
-        pitch=asin(dotProd(up,rocketUp));
+        pitch=asin(dotProd((float*)up,(float*)rocketUp));
     }
     pitchUp2Date = true;
     return pitch;
@@ -79,19 +79,31 @@ float rocket::getRoll(){
 
         float rocketNorth[3]={0};
         float rocketUp[3]={0};
+        float axisOfRot[3]={0};
 
         Matrix.Multiply((float *)R,(float *)up,3,3,1,(float*)rocketUp);
-        /*
-        take the cross product of rocketUp and up.  //The resulting vector points along the axis of rotation needed to digitally make the rocket point up.
-        get the angle between rocketUp and up using a cross b = a*b*sin(\theta)
-        normalize that vector.
-        using the angle and the normalized cross product, construct a quaternion.
-        using that quaternion, make a rotation matrix.
-        multiply the two rotation matricies to produce a new one //This will only rotate rocket north about up.
-        */
 
-        Matrix.Multiply((float *)R,(float *)north,3,3,1,(float*)rocketNorth);//TODO: swap R with the matrix mentioned above
-        Matrix.Print((float*)rocketNorth,3,1,"n");
+        cross((float*)up,(float*)rocketUp,(float*)axisOfRot);//May need to change the order of up and rocket up?
+        float angle2Vert=asin(vecMag((float*)axisOfRot,3));
+        normalize((float*)axisOfRot,(float*)axisOfRot);
+
+        //Calculate quaternion that will turn the rockets pointing vector straight up
+        float Qw=cos(angle2Vert/2);
+        float Qx=sin(angle2Vert/2)*axisOfRot[0];
+        float Qy=sin(angle2Vert/2)*axisOfRot[1];
+        float Qz=sin(angle2Vert/2)*axisOfRot[2];
+
+        float R2[9]={0};
+        float R3[9]={0}
+
+        R2[0] = 1 - 2 * (SQ(Qy) + SQ(Qz)); R2[1] = 2 * (Qx*Qy - Qz*Qw); R2[2] = 2 * s*(Qx*Qz+Qy*Qw);
+	    R2[3] = 2 * (Qx*Qy+Qz*Qw); R2[4] = 1 - 2 * (SQ(Qx) + SQ(q_z)); R2[5] = 2 * (Qy*Qz-Qx*Qw);
+	    R2[6] = 2 * (Qx*Qz + Qy * Qw); R2[7] = 2 * (Qy*Qz + Qx * Qw); R[8] = 1 - 2 * (SQ(Qx) + SQ(Qy));
+
+        Matrix.Multiply((float*)R,(float*)R2,3,3,3,(float*)R3);
+
+        Matrix.Multiply((float *)R3,(float *)north,3,3,1,(float*)rocketNorth);//TODO: swap R with the matrix mentioned above
+        Matrix.Print((float*)rocketNorth,3,1,"n"); //Remove after we've check that the vertical component is negligible.
 
         if (rocketNorth[1]==0){
             roll = rocketNorth[0]>0 ? PI/2.0 : (3.0/2.0)*PI;
@@ -114,7 +126,7 @@ int rocket::updateRotMatrix(){
         float s = (q == 0) ? 1 : (1.0 / q);
     
         R[0] = 1 - 2 * s*(SQ(q_y) + SQ(q_z)); R[1] = 2 * s*(q_x*q_y - q_z*q_w); R[2] = 2 * s*(q_x*q_z+q_y*q_w);
-	    R[3] = 2 * s*(q_x*q_y+q_z*q_w); R[4] = 1 - 2 * s*(Q[0] * Q[0] + Q[2] * Q[2]); R[5] = 2 * s*(q_y*q_z-q_x*q_w);
+	    R[3] = 2 * s*(q_x*q_y+q_z*q_w); R[4] = 1 - 2 * s*(SQ(q_x) + SQ(q_z)); R[5] = 2 * s*(q_y*q_z-q_x*q_w);
 	    R[6] = 2 * s*(q_x*q_z + q_y * q_w); R[7] = 2 * s*(q_y*q_z + q_x * q_w); R[8] = 1 - 2 * s*(SQ(q_x) + SQ(q_y));
       
     }    
