c15323b248050db43a4db7ca75b2e5e464b15947
Michael Garlak <mgarlak@kent.edu>
2018-02-25 16:54:36 -0500

    Listening in loop for a flightPlan

--- comms/comms.ino
+++ comms/comms.ino
@@ -1,5 +1,4 @@
-#include "utillity.hpp"
-#include <SoftwareSerial.h>
+#include "utility.hpp"
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
@@ -10,44 +9,49 @@
 #define fpacc 5
 #define controlDevice 75
 
-SoftwareSerial Xbee(2,3); //Rx, Tx
 File configf;
 File logger;
 int cmdSqnc = 0;
-bool wireFlag = false;
+char data[22];
+char* fp2 = nullptr;
+bool newConfig = true;
 
 void setup(){
-  	SD.begin(sdPin);
-	Xbee.begin(9600);
-	Serial.begin(9600);
-	Wire.begin(19);
+    SD.begin(sdPin);
+    Serial.begin(57600);
+    Wire.begin(19);
 
- 	Serial.print(F("Waiting For Instruction..."));
-  	Wire.onRequest(requestHandler);
+    Serial.print(F("Waiting For Instruction..."));
+    Wire.onRequest(requestHandler);
+    Wire.onReceive(receiveHandler);
 }
 
 void loop(){
-    // read data
-    Wire.requestFrom(controlDevice, 21);
-    char data[22];
-    data[22] = '\0';
-    int i = 0;
-    while (Wire.available() && i < 22){
-        data[i] = wire.read();
-        ++i;
+    if (newConfig){
+        while (Serial.available()){
+            fp2 = caAppend(fp2, Serial.read());
+        }
+        Serial.write(fp2);
+        delete[] fp2;
+        newConfig = false;
     }
-
-    logSD(data);
-    transmitXbee(data);
 }
 
 void requestHandler(){
-    Serial.print("IN REQUEST HANDLER");
     switch (cmdSqnc){
         case 0: ackSD(); break;
         case 1: sendParam(); break;
         default: {}
     }
+}
+
+void receiveHandler(int bytesReceived){
+    for (int i = 0; Wire.available() && i < 22; ++i){
+        data[i] = Wire.read();
+    }
+    Serial.println(str);
+    logSD(str);
+    transmitXbee(str);
 }
 
 void ackSD(){
@@ -63,25 +67,25 @@ void ackSD(){
 
 void sendParam(){
     Serial.print(F("Parsing a parameter..."));
-	if (configf){
-		char* str = nullptr;
-		while (1){
-			char ch = configf.read();
+    if (configf){
+        char* str = nullptr;
+        while (1){
+            char ch = configf.read();
             if (ch == -1) {
                 Serial.println(F("Done Parsing"));
                 ++cmdSqnc; 
                 break;
             }
-			else if (ch == '\n'){
+            else if (ch == '\n'){
                 Serial.print(F("Sending Parameter..."));
                 Wire.write(str);
                 delete[] str;
                 str = nullptr;
                 break;
-			}
-			else if (isFpVital(ch)){str=caAppend(str, ch);}
-		}
-	}
+            }
+            else if (isFpVital(ch)){str=caAppend(str, ch);}
+        }
+    }
 }
 
 /*Implement a function to move forward with the next cmdSqnc*/
